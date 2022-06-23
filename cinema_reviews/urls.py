1deb0faa3490f8f84f5c2923334bcde46184c0dd
Flopa M <flopamendez@gmail.com>
2022-06-23 15:33:24 -0300

    primeros views, templates y urls de reviews.app

--- cinema_reviews/urls.py
+++ cinema_reviews/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from reviews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reviews/', include('reviews.urls')),
+    # path('usuarios/', include('usuarios.urls')),
+    path("inicio/", views.reviews_inicio, name="inicio"),
+    path("about_us", views.about_us, name = 'about_us' ),
 ]
---
+++ reviews/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from reviews import views
+
+
+urlpatterns = [
+    
+    path("detalle/<pk>/", views.ReviewsDetail.as_view(), name ="review_detail"),
+    path("crear/", views.ReviewCreate.as_view(), name="review_create"),
+    path("editar/<pk>/", views.ReviewUpdate.as_view(), name ="review_update"),
+    path("borrar/<pk>/", views.ReviewDelete.as_view(), name ="review_delete"),
+    path('lista_reviews/',views.ReviewsList.as_view(), name="reviews_list"),
+]
